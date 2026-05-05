9584d53f2adaff8f2a27a4d548008556cf63a3a3
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-05-05 09:51:14 +0000

    feat: Add __init__.py with package exports

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from src.colors import *
+from src.fonts import *
+from src.highlighter import tokenize_code
+from src.renderer import render_code, render_terminal
+
+__all__ = [
+    "BG", "FG", "KW", "TY", "FN", "CM", "ST", "NU", "PP", "ORANGE",
+    "TERM_BG", "TERM_FG", "PROMPT_USR", "PROMPT_DIR", "CMD_COLOR",
+    "OUT_COLOR", "INPUT_COLOR", "RESULT_COLOR",
+    "load_font", "measure", "FONT_SIZE", "PAD", "LINE_H",
+    "tokenize_code",
+    "render_code", "render_terminal",
+]
